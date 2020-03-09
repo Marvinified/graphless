b0326371f4bf3e1e545de924796877142b6af604
Marvin Tunji-ola <marvinsmart9@gmail.com>
2020-03-09 14:30:11 +0100

    update and add devgateway

--- packages/gcloud/gateway.js
+++ packages/gcloud/gateway.js
@@ -1,25 +1,64 @@
 const { ApolloServer } = require("apollo-server-cloud-functions");
 const { ApolloGateway } = require("@apollo/gateway");
 const { getGraphJSON } = require("@graphless/core");
+module.exports = () => {
+  const graphs = getGraphJSON();
+  if (process.env.NODE_ENV === "production") {
+    const serviceList = graphs.map(graph => {
+      return {
+        name: graph.name,
+        // url: process.env.CLOUD_URL
+        url: process.env.CLOUD_URL + "/" + graph.name
+      };
+    });
 
-const graphs = getGraphJSON();
+    const gateway = new ApolloGateway({
+      serviceList
+    });
 
-const serviceList = graphs.map(graph => {
-  return {
-    name: graph.name,
-    url: process.env.CLOUD_URL + "/" + graph.name
-  };
-});
 
-const gateway = new ApolloGateway({
-  serviceList
-});
+    const server = new ApolloServer({
+      gateway,
+      subscriptions: false,
+      playground: true,
+      introspection: true,
+      debug: true
+    });
 
-const server = new ApolloServer({
-  gateway,
-  subscriptions: false,
-  playground: true,
-  introspection: true
-});
+    return server.createHandler();
 
-module.exports = server.createHandler();
+  }
+
+  return loadDevGateway()
+
+};
+
+
+function loadDevGateway() {
+  if (process.env.GRAPHLESS_LOAD_GATEWAY) {
+    const GRAPHLESS_LOCAL_GRAPHS_MAP = JSON.parse(process.env.GRAPHLESS_LOCAL_GRAPHS_MAP)
+    const serviceList = GRAPHLESS_LOCAL_GRAPHS_MAP.map(graph => {
+      return graph;
+    });
+
+    const gateway = new ApolloGateway({
+      serviceList
+    });
+
+
+    const server = new ApolloServer({
+      gateway,
+      subscriptions: false,
+      playground: true,
+      introspection: true,
+      debug: true
+    });
+
+    return server.createHandler();
+
+  }
+
+  return (req, res) => {
+    res.send("Not Ready")
+  }
+}
--- packages/gcloud/index.js
+++ packages/gcloud/index.js
@@ -2,6 +2,6 @@ const { ApolloServer, gql } = require("apollo-server-cloud-functions");
 const { registerGraphFunctions } = require("@graphless/core");
 const gateway = require("./gateway");
 
-exports.registerGraphFunctions = registerGraphFunctions(gateway);
+exports.registerGraphFunctions = registerGraphFunctions(gateway());
 exports.ApolloServer = ApolloServer;
 exports.gql = gql;
